e7facaef2f917f287c3b62c7a0f2829a78523dbc
kuolun <kuolun@gmail.com>
2018-03-18 17:10:03 +0800

    JS:add filter function

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const form = document.querySelector('#task-form');//上面from
 const taskInput = document.querySelector('#task');//new task input
 const taskList = document.querySelector('.collection');//ul
-const clearBtn = document.querySelector('#clear-tasks');
+const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');//下面的filter input
 
 
@@ -16,6 +16,12 @@ function loadEventListeners(){
 
     //add remove event (若click在ul內的element上的event會bubble up)
     taskList.addEventListener('click',removeTask);
+
+    //add click event
+    clearBtn.addEventListener('click',clearTasks);
+
+    //filter tasks
+    filter.addEventListener('keyup',filterTasks);
 }
 
 //add task
@@ -44,17 +50,51 @@ function addTask(e){
     //append li to ul
     taskList.appendChild(li);
 
+    //結構 ul>li>textNode+link
+
     // clear input
     taskInput.value = '';
 
     e.preventDefault();//避免form送出
 }
 
-//remove task
+//remove task   
 function removeTask(e){
     if(e.target.classList.contains('fa-remove')){
         if(confirm('Are you sure')){
             e.target.parentElement.parentElement.remove();
         }
     }
+}
+
+//clear tasks
+function clearTasks(){
+    //將ul下的innerHTML清空
+    // taskList.innerHTML='';
+
+    //cehck ul下是否還有child,有就持續remove
+    while(taskList.firstChild){
+        console.log(taskList.firstChild);
+        taskList.removeChild(taskList.firstChild);
+    }
+
+}
+
+
+//filter tasks
+function filterTasks(e){
+    console.log(e.target.value);
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function(task){
+        console.log(task.textContent);
+        //check task中是否有包含text,要轉小寫去比較
+        const task_text = task.textContent;
+        if(task_text.toLocaleLowerCase().indexOf(text) !== -1){
+            task.style.display = 'block';
+        }else{
+            task.style.display = 'none';
+        }
+    });
+
 }
