42785e4dd447906cfe86c4e1455f751d09643642
kuolun <kuolun@gmail.com>
2018-03-18 18:19:15 +0800

    JS:save/load localStorage

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ function loadEventListeners(){
 
     //filter tasks
     filter.addEventListener('keyup',filterTasks);
+
+    //load tasks from local storage
+    document.addEventListener('DOMContentLoaded',loadContent);
 }
 
 //add task
@@ -55,14 +58,36 @@ function addTask(e){
     // clear input
     taskInput.value = '';
 
+    //add task to localStorage (加入前先檢查是否已存在key='tasks')
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(li.textContent);
+    console.log(tasks);
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+
     e.preventDefault();//避免form送出
 }
+
 
 //remove task   
 function removeTask(e){
     if(e.target.classList.contains('fa-remove')){
         if(confirm('Are you sure')){
             e.target.parentElement.parentElement.remove();
+            let text = e.target.parentElement.parentElement.textContent;
+            let tasks = JSON.parse(localStorage.getItem('tasks'));
+            tasks.forEach(function(task,index){
+                console.log('text:',text);
+                console.log('task:',task);
+                if(text === task){
+                    tasks.splice(index,1);
+                }
+            });
+            localStorage.setItem('tasks',JSON.stringify(tasks));
         }
     }
 }
@@ -77,6 +102,7 @@ function clearTasks(){
         console.log(taskList.firstChild);
         taskList.removeChild(taskList.firstChild);
     }
+    localStorage.clear();
 
 }
 
@@ -97,4 +123,35 @@ function filterTasks(e){
         }
     });
 
+}
+
+//load tasks from localStorage
+function loadContent(){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+        tasks.forEach(function(task){
+            //ul要用collection
+            //li用collection-item在materialize會自動套樣式
+            const li = document.createElement('li');
+            li.className = 'collection-item';
+
+            //create textNode and append to li
+            li.appendChild(document.createTextNode(task));
+
+            //create delete link
+            const link = document.createElement('a');
+            // 如果要在li裡面有東西靠右要用.secondary-content
+            link.className='delete-item secondary-content'
+            link.innerHTML = '<i class="fa fa-remove"></i>';
+
+            //append link to li
+            li.appendChild(link);
+        
+            //append li to ul
+            taskList.appendChild(li);
+        })
+    }
 }
